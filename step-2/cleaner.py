ea818de1e586848a719b8eb293c66d65ae68e875
Abhi Kishore <abhi.kishore95@gmail.com>
2019-12-16 19:26:57 +1100

    python-workshop: cleanup, style.

--- step-2/cleaner.py
+++ step-2/cleaner.py
@@ -2,6 +2,8 @@ from shutil import copyfile
 from re import search, compile
 from json import dumps
 
+# Name of file where formatted lines are to be placed:
+FILE_NAME = 'server.log'
 # Regex paths for logging:
 REGEX_TIME = r'\w+:127.0.0.1.*?\[(?P<date>.*?) (?P<timestamp>.*?)\] '
 REGEX_REQUEST = r'\"(?P<http_method>.*?) (?P<http_path>.*?) (?P<http_protocol>.*?)\" '
@@ -9,14 +11,16 @@ REGEX_RESPONSE = r'(?P<response_status>\d+).*?'
 REGEX_LINE = compile("%s%s%s" % (REGEX_TIME, REGEX_REQUEST, REGEX_RESPONSE))
 
 
-def setup():
-    copyfile('../step-1/server.log', 'server.log')
+# Copy over the server log file from the application directory.
+def setup(src_file):
+    copyfile(src_file, FILE_NAME)
 
 
-def getLines(srcFile):
+# Parses a log file and returns lines which match the expected regex.
+def getLines(src_file):
     lines = []
     # Open source file and filter application lines.
-    with open(srcFile) as logFile:
+    with open(src_file) as logFile:
         for line in logFile:
             matches = search(REGEX_LINE, line)
             if matches:
@@ -26,12 +30,13 @@ def getLines(srcFile):
     return lines
 
 
-def writeLines(destFile, lines):
-    file = open(destFile, 'w')
+# Writes formatted lines to a dest file/directory.
+def write_lines(dest_file, lines):
+    file = open(dest_file, 'w')
     file.write(dumps(lines))
     file.close()
 
 
 if __name__ == '__main__':
-    setup()
-    writeLines('../step-3/logs.json', getLines('server.log'))
+    setup('../step-1/server.log')
+    write_lines('../step-3/logs.json', getLines(FILE_NAME))
